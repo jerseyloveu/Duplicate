0f5610a6ce650f44389d602673a5d3ca2395560f
Jersey Love Usman <usmanjt@students.nu-moa.edu.ph>
2025-04-12 20:43:11 +0800

    navbar created

--- juanems-user/user_frontend/user_frontend/src/components/HomePage.js
+++ juanems-user/user_frontend/user_frontend/src/components/HomePage.js
@@ -1,12 +1,37 @@
-// src/components/HomePage.js
 import React from 'react';
-import '../css/HomePage.css'; // Importing the CSS file
+import { FaRegSmileWink } from 'react-icons/fa';
+import '../css/HomePage.css';
+import SJDEFILogo from '../images/SJDEFILogo.png';
 
 function HomePage() {
   return (
     <div className="home-container">
-      <h1>Welcome to the Home Page!</h1>
-      <p>This is where your content will go.</p>
+      {/* Header */}
+      <header className="homepage-header">
+        <div className="header-left">
+          <img 
+            src={SJDEFILogo} 
+            alt="SJDEFI Logo" 
+            className="logo" 
+          />
+          <div className="header-text">
+            <h1>SAN JUAN DE DIOS EDUCATIONAL FOUNDATION, INC.</h1>
+            <p className="motto">Where faith and reason are expressed in Charity.</p>
+          </div>
+        </div>
+        <div className="header-right">
+          <p>
+            Welcome to <strong>JuanEMS</strong>{' '}
+            <FaRegSmileWink className="icon" />
+          </p>
+        </div>
+      </header>
+
+      {/* Main Content */}
+      <main className="homepage-main">
+        <h2>Welcome to the Home Page!</h2>
+        <p>This is where your content will go.</p>
+      </main>
     </div>
   );
 }
